a7055ead07a344ead629343c6a411e53bedc5c29
Ryan McNeil <ryan@journeyapps.com>
2019-12-17 16:11:57 -0700

    Updates

---
+++ precache-manifest.20dd476335cce7295882f778f83cdd44.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "052323cf96ae42dbe4d543160686768a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "0fca300b8528320b970d",
+    "url": "/monsters-rolodex/static/css/main.775d10b9.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0fca300b8528320b970d",
+    "url": "/monsters-rolodex/static/js/main.537ce985.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
