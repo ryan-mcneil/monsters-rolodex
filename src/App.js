ab22290725460106acbedbbffdea403016315312
Ryan McNeil <ryan@journeyapps.com>
2019-12-20 11:02:04 -0700

    Update title

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ class App extends Component {
     )
     return (
       <div className="App">
-      <h1>Mosters Rolodex</h1>
+      <h1>Monsters Rolodex</h1>
 
         <SearchBox
           placeholder='search monsters'
